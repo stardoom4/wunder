f76dc620872d05e1fe73d225d572959c082909ad
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-12-01 11:23:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+OUTPUT_DIR="build"
+
 rm -rf go-blender
 git clone https://github.com/awalvie/go-blender
 cd go-blender && make build && cd ..
@@ -10,4 +12,6 @@ if [[ -e ./build ]];
 fi
 
 mkdir build
+cp home.html "$OUTPUT_DIR"
+echo "home.html has been added to $OUTPUT_DIR."
 ./blender build ./
